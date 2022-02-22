c80fc2300632df73065ee1b3195a4496a305dd00
denonia <denoniaa@gmail.com>
2022-02-22 20:21:51 +0200

    Cleaned up Program.cs

--- IronPythonASP/Program.cs
+++ IronPythonASP/Program.cs
@@ -1,5 +1,5 @@
 using IronPython.Hosting;
-using Microsoft.Scripting.Hosting;
 
-ScriptEngine engine = Python.CreateEngine();
-engine.ExecuteFile("main.py");
+Python
+    .CreateEngine()
+    .ExecuteFile("main.py");
